aed72804b7ee9d3a0e2240e92c7ea39a2e5ec8a5
Jason C <cumiskey@camosun.bc.ca>
2022-12-13 09:52:59 -0800

    add knob

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,31 +1,37 @@
-import { useState } from "react";
+import { Knob } from 'primereact/knob';
 
 
 const Card = ({ numInHand, updateNumCards, id, image, max }) => {
   
 
-  const handleSelect = (e) => {
-    const numCards = parseInt(e.target.value, 10);
-    updateNumCards(id, numCards);
-  }
+  // const handleSelect = e => {
+  //   const numCards = parseInt(e.target.value, 10);
+  //   updateNumCards(id, numCards);
+  // }
 
 
-  const buildOptions = () => {
-    const options = [];
+  // const buildOptions = () => {
+  //   const options = [];
 
-    for (let i=0; i < max + 1; i++) {
-      options[i] = <option key={i} value={i}>{i}</option>
-    }
+  //   for (let i=0; i < max + 1; i++) {
+  //     options[i] = <option key={i} value={i}>{i}</option>
+  //   }
 
-    return options;
-  }
+  //   return options;
+  // }
 
   return(
     <div className="Card">
       <img alt="card" src={image} style={{ marginBottom: '10px' }} />
-      <select value={numInHand[id]} id={id} onChange={handleSelect}>
+      {/*<select value={numInHand[id]} id={id} onChange={handleSelect}>
         { buildOptions() }
-      </select>
+        </select>*/}
+      <Knob value={numInHand[id]}
+        min={0} max={max} size={75}
+        valueColor={"MediumBlue"}
+        rangeColor={"DarkGrey"}
+        textColor={"AliceBlue"}
+        onChange={(e) => updateNumCards(id,e.value)} />  
     </div>
   );
 }
